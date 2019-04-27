09f774b708ae5f7563d14e74ba121748833ff6cc
xcnkx <cnk_2806@me.com>
2019-04-27 13:47:23 +0900

    add make_dataset.py

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,30 +1,54 @@
 # -*- coding: utf-8 -*-
-import click
-import logging
-from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+import subprocess
+import feather
+import pandas as pd
+import os
+import pathlib
 
+class Dataset():
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+  def __init__(self, comp_name=None, d_path='./data/'):
+    self._comp_name = comp_name
+    self._d_path = pathlib.Path(d_path)
 
+  def download_data(self):
+        #Download dataset from kaggle using kaggle api
+    try:
+        subprocess.check_call("kaggle competitions download -c {} -p ./data/raw/".format(self._comp_name))
+    except:
+        print("subprocess.check_call() failed")
 
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
+  def get_train_test(self):
+    raw_p = self._d_path / 'raw'                 #raw dir path
+    self._trf_l = list(raw_p.glob('*.ftr'))      #feather files list
+    self._csv_l = list(raw_p.glob('*.csv'))      #csv files list
 
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
+    if(len(self._trf_l) == 0):
 
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
+      if(len(self._csv_l) != 0):
+        train_df = pd.read_csv("./data/raw/train.csv")
+        test_df = pd.read_csv("./data/raw/test.csv")
+      else:
+        print('csv file not exist')
 
-    main()
+      feather.write_dataframe(train_df, "./data/raw/train.ftr")
+      feather.write_dataframe(test_df, "./data/raw/test.ftr")
+      print('Feather file created')
+
+    else:
+      train_df = feather.read_dataframe("./data/raw/train.ftr")
+      test_df = feather.read_dataframe("./data/raw/test.ftr")
+
+    return train_df, test_df
+
+  def get_trf_list(self):
+    return self._trf_l
+
+  def get_csv_list(self):
+    return self._csv_l
+
+  def get_comp_name(self):
+    return self._comp_name
+
+  def get_data_path(self):
+    return str(self._d_path)
